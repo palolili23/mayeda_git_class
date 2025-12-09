62952ccbfd0b34d7947fbd7581c73758d3455ea4
15shethami <amisheth26@g.ucla.edu>
2025-12-09 12:58:45 -0800

    test in github for ami_test.R

---
+++ ami_test.R
@@ -0,0 +1,16 @@
+# load libraries
+library(tidyverse)
+library(MASS)
+
+# load data
+data(iris)
+head(iris)
+
+# visualize data
+plot <- iris %>% 
+  ggplot(aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
+  geom_point()
+
+# run regression
+lm <- glm(Sepal.Length ~ Sepal.Width + factor(Species), data = iris)
+summary(lm)
